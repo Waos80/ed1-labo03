e4fce231908c53d7a13a9791bfe2cf2ad3bc061c
wis <wis@wis>
2025-05-16 20:47:54 -0600

    Tests

--- src/main/java/ed/lab/E01TopKFrequentElements.java
+++ src/main/java/ed/lab/E01TopKFrequentElements.java
@@ -1,7 +1,30 @@
 package ed.lab;
 
+import java.util.*;
+
 public class E01TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
-        return null;
+       HashMap<Integer, Integer> map = new HashMap<>();
+       for (int num : nums) {
+           map.merge(num, 1, Integer::sum);
+       }
+
+       Queue<Map.Entry<Integer, Integer>> q = new PriorityQueue<>((a, b) -> {
+           return a.getValue() - b.getValue() ;
+       });
+
+        for (Map.Entry<Integer, Integer> i : map.entrySet()) {
+           q.offer(i);
+           if (q.size() > k) {
+               q.poll();
+           }
+        }
+
+        int[] res = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            res[i] = q.poll().getKey();
+        }
+
+        return res;
     }
 }
--- src/main/java/ed/lab/E02AutocompleteSystem.java
+++ src/main/java/ed/lab/E02AutocompleteSystem.java
@@ -1,13 +1,59 @@
 package ed.lab;
-import java.util.List;
+import java.util.*;
 
 public class E02AutocompleteSystem {
+    private HashMap<String, Integer> map;
+    private StringBuilder builder;
+
+    private List<String> getMostFrequent(String str) {
+        ArrayList<Map.Entry<String, Integer>> entry_matches = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getKey().startsWith(str)) {
+                entry_matches.add(entry);
+            }
+        }
+
+        Queue<Map.Entry<String, Integer>> heap = new PriorityQueue<>((e1, e2) -> {
+            if (e1.getValue().equals(e2.getValue())) {
+                return e1.getKey().compareTo(e2.getKey());
+            }
+            return e2.getValue().compareTo(e1.getValue());
+        });
+
+        heap.addAll(entry_matches);
+
+        if (heap.isEmpty()) {
+            return List.of();
+        }
+
+        ArrayList<String> most_freq = new ArrayList<>();
+        int i = 0;
+        while (i < 3 && !heap.isEmpty()) {
+            most_freq.add(heap.poll().getKey());
+            i++;
+        }
+
+        return most_freq;
+    }
 
     public E02AutocompleteSystem(String[] sentences, int[] times) {
+
+        this.map = new HashMap<>();
+        this.builder = new StringBuilder();
+        for (int i = 0; i < sentences.length; i++) {
+            this.map.put(sentences[i], times[i]);
+        }
 
     }
 
     public List<String> input(char c) {
-        return null;
+        if (c == '#') {
+            map.merge(builder.toString(), 1, Integer::sum);
+            builder = new StringBuilder();
+            return List.of();
+        }
+
+        builder.append(c);
+        return getMostFrequent(builder.toString());
     }
 }
